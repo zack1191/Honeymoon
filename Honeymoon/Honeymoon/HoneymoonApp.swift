b7a690339e04d38dcd73186d6901de264db870a8
Hein Htet Aung <>
2024-06-26 13:50:15 +0900

    add launch screen

---
+++ Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Hein Htet Aung on 2024/06/26.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
